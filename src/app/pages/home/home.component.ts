6b3d9931acaeca22dc6731b470d845dc2b333b01
Kanky9 <bournotsantino03@gmail.com>
2024-08-15 12:34:51 -0300

    add presentation

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PresentationComponent } from '../../shared/components/presentation/presentation.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    PresentationComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ src/app/shared/components/presentation/presentation.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-presentation',
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
+  templateUrl: './presentation.component.html',
+  styleUrl: './presentation.component.scss'
+})
+export class PresentationComponent {
+
+}
