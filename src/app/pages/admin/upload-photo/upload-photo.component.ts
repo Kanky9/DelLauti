a666de2622bd64d5c5e01fd0f5fe6e0a22f6774e
Kanky <bournotsantino03@gmail.com>
2025-02-25 14:57:05 -0300

    comment upload photo

--- src/app/pages/admin/upload-photo/upload-photo.component.ts
+++ src/app/pages/admin/upload-photo/upload-photo.component.ts
@@ -12,76 +12,76 @@ import { LoadingComponent } from '../../../shared/utils/loading/loading.componen
   standalone: true,
   imports: [
     CommonModule,
-    RouterLink,
+    // RouterLink,
     FormsModule,
     ReactiveFormsModule,
-    LoadingComponent
+    // LoadingComponent
   ],
   templateUrl: './upload-photo.component.html',
   styleUrl: './upload-photo.component.scss'
 })
 export class UploadPhotoComponent {
-  @ViewChild('fileInput') fileInput!: ElementRef;
+  // @ViewChild('fileInput') fileInput!: ElementRef;
 
-  photoForm: FormGroup;
-  selectedFile: Signal<File | null> = signal(null);
-  selectedFileUrl: Signal<string | null> = signal(null);
-  categories = ['Diseño', 'Clásico', 'Cresta', 'Degradé', 'Degradé en punta'];
+  // photoForm: FormGroup;
+  // selectedFile: Signal<File | null> = signal(null);
+  // selectedFileUrl: Signal<string | null> = signal(null);
+  // categories = ['Diseño', 'Clásico', 'Cresta', 'Degradé', 'Degradé en punta'];
 
-  private _fb = inject(FormBuilder);
-  private _uploadPhotoService = inject(UploadPhotoService);
-  private _snackBar = inject(MatSnackBar);
-  readonly _utilSvc = inject(UtilsService);
+  // private _fb = inject(FormBuilder);
+  // private _uploadPhotoService = inject(UploadPhotoService);
+  // private _snackBar = inject(MatSnackBar);
+  // readonly _utilSvc = inject(UtilsService);
 
-  constructor() {
-    this.photoForm = this._fb.group({
-      photo: ['', Validators.required],
-      category: ['', Validators.required]
-    });
-  }
+  // constructor() {
+  //   this.photoForm = this._fb.group({
+  //     photo: ['', Validators.required],
+  //     category: ['', Validators.required]
+  //   });
+  // }
 
-  triggerFileInputClick() {
-    this.fileInput.nativeElement.click();
-  }
+  // triggerFileInputClick() {
+  //   this.fileInput.nativeElement.click();
+  // }
 
-  onFileSelected(event: Event) {
-    const input = event.target as HTMLInputElement;
-    if (input.files && input.files.length > 0) {
-      const file = input.files[0];
-      this.selectedFile = signal(file);
-      const reader = new FileReader();
-      reader.onload = () => {
-        this.selectedFileUrl = signal(reader.result as string);
-      };
-      reader.readAsDataURL(file);
-    }
-  }
+  // onFileSelected(event: Event) {
+  //   const input = event.target as HTMLInputElement;
+  //   if (input.files && input.files.length > 0) {
+  //     const file = input.files[0];
+  //     this.selectedFile = signal(file);
+  //     const reader = new FileReader();
+  //     reader.onload = () => {
+  //       this.selectedFileUrl = signal(reader.result as string);
+  //     };
+  //     reader.readAsDataURL(file);
+  //   }
+  // }
 
-  async onSubmit() {
-    if (this.photoForm.valid && this.selectedFile()) {
-      this._utilSvc.show(); // Mostrar el loading
-      try {
-        const file = this.selectedFile();
-        if (file) {
-          await this._uploadPhotoService.uploadPhoto(file, this.photoForm.value.category);
-          this._snackBar.open('La imagen se subió correctamente', 'Cerrar', {
-            duration: 3000,
-          });
-          this.photoForm.reset();
-          this.selectedFile = signal(null);
-          this.selectedFileUrl = signal(null);
-        }
-      } catch (error) {
-        console.error('Error al subir la imagen', error);
-        this._snackBar.open('Hubo un error al subir la imagen', 'Cerrar', {
-          duration: 3000,
-        });
-        this.photoForm.reset();
-        this.selectedFile = signal(null);
-        this.selectedFileUrl = signal(null);
-      } finally {
-        this._utilSvc.hide(); // Ocultar el loading
-      }
-    }
-  }
+  // async onSubmit() {
+  //   if (this.photoForm.valid && this.selectedFile()) {
+  //     this._utilSvc.show(); // Mostrar el loading
+  //     try {
+  //       const file = this.selectedFile();
+  //       if (file) {
+  //         await this._uploadPhotoService.uploadPhoto(file, this.photoForm.value.category);
+  //         this._snackBar.open('La imagen se subió correctamente', 'Cerrar', {
+  //           duration: 3000,
+  //         });
+  //         this.photoForm.reset();
+  //         this.selectedFile = signal(null);
+  //         this.selectedFileUrl = signal(null);
+  //       }
+  //     } catch (error) {
+  //       console.error('Error al subir la imagen', error);
+  //       this._snackBar.open('Hubo un error al subir la imagen', 'Cerrar', {
+  //         duration: 3000,
+  //       });
+  //       this.photoForm.reset();
+  //       this.selectedFile = signal(null);
+  //       this.selectedFileUrl = signal(null);
+  //     } finally {
+  //       this._utilSvc.hide(); // Ocultar el loading
+  //     }
+  //   }
+  // }
 }
--- src/app/services/upload-photo.service.ts
+++ src/app/services/upload-photo.service.ts
@@ -8,40 +8,40 @@ import { finalize } from 'rxjs';
 })
 export class UploadPhotoService {
 
-  private _firestore = inject(Firestore);
-  private _storage = inject(Storage);
+  // private _firestore = inject(Firestore);
+  // private _storage = inject(Storage);
 
-  constructor() { }
+  // constructor() { }
 
-  uploadPhoto(file: File, category: string): Promise<void> {
-    const filePath = `photos/${category}/${file.name}`;
-    const storageRef = ref(this._storage, filePath);
-    const uploadTask = uploadBytesResumable(storageRef, file);
+  // uploadPhoto(file: File, category: string): Promise<void> {
+  //   const filePath = `photos/${category}/${file.name}`;
+  //   const storageRef = ref(this._storage, filePath);
+  //   const uploadTask = uploadBytesResumable(storageRef, file);
 
-    return new Promise((resolve, reject) => {
-      uploadTask.on(
-        'state_changed',
-        null,
-        (error) => reject(error),
-        async () => {
-          try {
-            const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
-            await this.savePhotoInfo(file.name, category, downloadURL);
-            resolve();
-          } catch (error) {
-            reject(error);
-          }
-        }
-      );
-    });
-  }
+  //   return new Promise((resolve, reject) => {
+  //     uploadTask.on(
+  //       'state_changed',
+  //       null,
+  //       (error) => reject(error),
+  //       async () => {
+  //         try {
+  //           const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+  //           await this.savePhotoInfo(file.name, category, downloadURL);
+  //           resolve();
+  //         } catch (error) {
+  //           reject(error);
+  //         }
+  //       }
+  //     );
+  //   });
+  // }
 
-  private savePhotoInfo(fileName: string, category: string, url: string) {
-    const photosCollection = collection(this._firestore, 'photos');
-    return addDoc(photosCollection, {
-      name: fileName,
-      category: category,
-      url: url
-    });
-  }
+  // private savePhotoInfo(fileName: string, category: string, url: string) {
+  //   const photosCollection = collection(this._firestore, 'photos');
+  //   return addDoc(photosCollection, {
+  //     name: fileName,
+  //     category: category,
+  //     url: url
+  //   });
+  // }
 }
